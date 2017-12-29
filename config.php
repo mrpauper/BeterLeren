19a91daa164c950945c8c61fb89d1fd48b2eee02
Daan Schrijver <daanschrijver1@gmail.com>
2017-12-29 13:27:20 +0100

    fixed gitignore stuff

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$con = new mysqli("localhost", "root", "Vlaflip43", "login");
-if ($con->connect_error) {
-    die("Connection failed: " . $con->connect_error);
-}
-
-?>
--- config2.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con = mysqli_connect("localhost", "root", "Vlaflip43", "wordlists");
-if (mysqli_connect_errno()){
-  die("Failed to connect to MySQL: " . mysqli_connect_error());
-}
-?>
